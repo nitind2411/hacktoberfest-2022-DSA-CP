d50a669841e21017144630bc9f2e703db3327dcc
rishabh7das <116669266+rishabh7das@users.noreply.github.com>
2022-10-26 00:14:38 +0530

    Create Nested_recursion.c

---
+++ Nested_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int fun(int n)
+{
+    if(n>100)
+    return n-10;
+    return fun(fun(n+11));
+}
+int main()
+{
+    int r;
+    r=fun(65);
+    printf("%d\n",r);
+    return 0;
+}
